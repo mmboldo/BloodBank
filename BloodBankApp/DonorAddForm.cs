24a1bbddb61fdfee250479c2a8929f5e8c5e45ca
Marcelo Meirelles Boldo <marcelo.meirelles@gmail.com>
2020-12-01 19:41:00 -0800

    Recreated the forms with the correct structure.

---
+++ BloodBankApp/DonorAddForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EFControllerUtilities;
+using BloodBankCodeFirstFromDB;
+
+namespace BloodBankApp
+{
+    public partial class DonorAddForm : Form
+    {
+        public DonorAddForm()
+        {
+            InitializeComponent();
+            this.Load += DonorAddForm_Load;
+            buttonSubmit.Click += ButtonSubmit_Click;
+        }
+
+        private void DonorAddForm_Load(object sender, EventArgs e)
+        {
+            listBoxBloodType.DataSource = Controller<BloodBankEntities, BloodType>.SetBindingList();
+            listBoxBloodType.SelectedIndex = -1;
+        }
+        private void ButtonSubmit_Click(object sender, EventArgs e)
+        {
+
+            BloodBankEntities context = new BloodBankEntities();
+            Donor donor = new Donor()
+            {
+                DonorFirstName = textBoxFirstName.Text,
+                DonorLastName = textBoxLastName.Text,
+                DonorBirthday = dateTimePickerBirthday.Value,
+                DonorAddress = textBoxAddress.Text,
+                DonorPhone = numericUpDown1.Value.ToString(),
+                BloodTypeId = listBoxBloodType.SelectedIndex + 1,
+            };
+            context.Dispose();
+            if (!(listBoxBloodType.SelectedItem is BloodType type))
+            {
+                MessageBox.Show("Must select a blood type!");
+                return;
+            }
+            if (donor.DonorFirstName.Trim().Length == 0 || donor.DonorFirstName.Trim().Length == 0 || donor.DonorBirthday == new DateTime(1900, 01, 01) || donor.DonorAddress.Trim().Length == 0 || donor.DonorPhone == "0" || donor.DonorPhone == null)
+            {
+                MessageBox.Show("Must fill all fields");
+                return;
+            }
+            if (!donor.DonorAddress.Contains("@") || !donor.DonorAddress.Contains("."))
+            {
+                MessageBox.Show("Email is not valid");
+                return;
+            }
+            if (Controller<BloodBankEntities, Donor>.AddEntity(donor) == null)
+            {
+                MessageBox.Show("Cannot add course to database");
+                return;
+            }
+            this.DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
---
+++ BloodBankApp/WithdrawalForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EFControllerUtilities;
+using BloodBankCodeFirstFromDB;
+namespace BloodBankApp
+{
+    public partial class WithdrawalForm : Form
+    {
+        public WithdrawalForm()
+        {
+            InitializeComponent();
+            this.Load += WithdrawForm_Load;
+
+            button1.Click += ButtonWithdraw_Click;
+            dataGridViewStock.SelectionChanged += (s, e) => GetSelection();
+        }
+        private void WithdrawForm_Load(object sender, EventArgs e)
+        {
+            dataGridViewStock.DataSource = Controller<BloodBankEntities, BloodDeposit>.SetBindingList();
+
+            dataGridViewClient.DataSource = Controller<BloodBankEntities, Client>.SetBindingList();
+            dataGridViewStock.Columns.Remove("BloodType");
+            dataGridViewStock.Columns.Remove("BloodWithdrawalUnits");
+            dataGridViewClient.Columns.Remove("BloodWithdrawals");
+        }
+
+        private void ButtonWithdraw_Click(object sender, EventArgs e)
+        {
+            BloodBankEntities context = new BloodBankEntities();
+            BloodWithdrawal bw = new BloodWithdrawal()
+            {
+                BloodWithdrawalId = context.BloodWithdrawals.Count() + 1,
+                BloodWithdrawalDate = DateTime.Now.Date,
+                TransactionValue = float.Parse(textBoxTotal.Text),
+                UnitQuantity = 1,
+                ClientId = Int32.Parse(dataGridViewClient.SelectedRows[0].Cells[0].Value.ToString())
+            };
+            if (dataGridViewStock.SelectedRows.Count < 1)
+            {
+                MessageBox.Show("Please select an item to withdraw");
+                return;
+            }
+            if (dataGridViewClient.SelectedRows.Count < 1)
+            {
+                MessageBox.Show("Please select a client for withdrawal");
+                return;
+            }
+
+            if (Controller<BloodBankEntities, BloodWithdrawal>.AddEntity(bw) == null)
+            {
+                MessageBox.Show("Cannot add withdrawal to database");
+                return;
+            }
+
+            BloodWithdrawalUnit bwu = new BloodWithdrawalUnit()
+            {
+                UnitId = Int32.Parse(dataGridViewStock.SelectedRows[0].Cells[0].Value.ToString()),
+                BloodWithdrawalId = context.BloodWithdrawals.Count(),
+            };
+            context.Dispose();
+            if (Controller<BloodBankEntities, BloodWithdrawalUnit>.AddEntity(bwu) == null)
+            {
+                MessageBox.Show("Cannot add withdrawal to database");
+                return;
+            }
+            this.DialogResult = DialogResult.OK;
+            Close();
+        }
+        private void GetSelection()
+        {
+            foreach (DataGridViewRow row in dataGridViewStock.SelectedRows)
+            {
+                textBoxTotal.Text = row.Cells["UnitPrice"].Value.ToString();
+            }
+        }
+    }
+}
