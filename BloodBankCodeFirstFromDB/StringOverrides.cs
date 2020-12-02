e6a3aad76e814516bdd75a741c2ac31a89ae9d5d
Marcelo Meirelles Boldo <marcelo.meirelles@gmail.com>
2020-12-01 18:28:33 -0800

    Added the StringOverrides.cs files.

---
+++ BloodBankCodeFirstFromDB/StringOverrides.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBankCodeFirstFromDB
+{
+    partial class BloodType
+    {
+        public override string ToString()
+        {
+            return BloodType1;
+        }
+    }
+}
